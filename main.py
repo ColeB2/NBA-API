cfdf66205e305499c99020333dd40509acb6ae2a
Cole <cebCole@gmail.com>
2020-02-03 18:10:59 -0600

    main initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+import urllib.request
+#import configapp
+
+import os, sys
+print(sys.path)
+from nba_boxscore_ui import BoxScore
+from api_functions import nbagameboxscore
+
+
+if __name__ == '__main__':
+    pass
