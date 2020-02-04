4469ff6e9ea8b6ad2b4052db28cb94e12798390c
Cole <cebCole@gmail.com>
2020-02-03 20:00:28 -0600

    UI functions inital commit

--- ui/ui_functions.py
+++ ui/ui_functions.py
@@ -8,16 +8,6 @@ class Widget():
     def __init__(self):
         pass
 
-    def pretty_print(obj, depth):
-        '''Used to limit the length of strings
-        params:
-        obj   : information needed to be printed.
-        depth : length limit of the created string.
-
-        prints: string which is cut off at the depth
-        '''
-        print(str(obj)[:depth], end='|')
-
 
 
 
@@ -40,8 +30,10 @@ def get_last_gameId(season=None, team=None):
     if team == None:
         team = get_team()
 
+
     url_start = 'http://data.nba.net/prod/v1/'
     url = url_start + str(season) + '/teams/' + str(team) + '/schedule.json'
+
 
     data = get_data(url)
     gameId = 'No Game on '
@@ -52,5 +44,42 @@ def get_last_gameId(season=None, team=None):
     gameId = y[last_game]['gameId']
     game_date = y[last_game]['gameUrlCode'][0:8]
     return gameId, game_date
+
+def get_gameId(date=20200120, season=None, team=None):
+    '''
+    get_gameId(date, season, team)
+
+
+    PARAMS : get_gameId(date, season, team)
+    date   : YYYYMMDD format ex: 20200120 for 2020 January 20th
+    season : YYYY format, based on season start date, 2019 for 2019/20 season
+    team   : lowercase team name, ex: raptors, sixers
+    example: get_gameId(20200120, 2019, 'raptors')
+
+    RETURNS:
+    gameId    : numerical id for the game at given date
+    game_date : date inputed for given game
+    example   : (0021900640, 20200120)
+    '''
+    if season == None:
+        season = get_season()
+    if team == None:
+        team = get_team()
+
+    url_start = 'http://data.nba.net/prod/v1/'
+    url = url_start + str(season) + '/teams/' + str(team) + '/schedule.json'
+
+
+    data = get_data(url)
+    gameId = 'No Game on ' + str(date)
+
+
+    x = data['league']
+    y = x['standard'] #List of all game information
+
+    for i in range(len(y)):
+        if str(date) == y[i]['gameUrlCode'][0:8]:
+            gameId = y[i]['gameId']
+    return gameId, date
 
 """
