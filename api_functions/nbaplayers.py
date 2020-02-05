6521995e83617c0f3f6000e983588226179cc9a3
Cole <cebCole@gmail.com>
2020-02-04 19:57:20 -0600

    + NBA player info class

--- api_functions/nbaplayers.py
+++ api_functions/nbaplayers.py
@@ -4,39 +4,60 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join('.', 'config')))
 from getconfiginfo import get_season
 
-def get_player_name(personId):
-    '''
-    Gets player name via personId
+def get_player_info(season=None):
+    """Gets raw json data for all players
+    """
+    if not season: season = get_season()
 
-    param    : get_player_name(personId)
-    personId : numerical code for the player ex '1627783' for Pascal Siakam
-    example  : get_player_name('1627783')
-
-    returns  : first and last name of the player as a string
-    example  : 'Pascal Siakam'
-    '''
-    season_year = get_season()
     url_start = 'http://data.nba.net/prod/v1/'
-    url_end = '/players.json'
-    url = str(url_start) + str(season_year) + str(url_end)
+    url = str(url_start) + str(season) + '/players.json'
+
+    data = get_data(url)
+
+    return data
+
 
     player_name = str()
-
     data = get_data(url)
     x = data['league']
     y = x['standard']
-
     for i in range(len(y)):
         if y[i]['personId'] == personId:
             first_name = str(y[i]['firstName'])
             last_name = str(y[i]['lastName'])
             player_name = first_name + ' ' + last_name
-
-
     return player_name
+
+class PlayerInfo(object):
+    """A class to sort and hold data for NBA players.json enpoint
+
+    Attributes:
+    """
+
+    def __init__(self):
+        self.raw_data = get_player_info()
+        self._internal = self.raw_data['_internal']
+
+        self.league = self.raw_data['league']
+
+        self.standard = self.league['standard']
 
 
 if __name__ == '__main__':
-    #'1627783' is personId for Pascal Siackam
-    player_name = get_player_name('1627783')
-    print(player_name)
+    print('Creating Player Info')
+    PI = PlayerInfo()
+    print('-----RAW DATA BREAKDOWN, DICT KEYS---------------------------------')
+    #print(PI.raw_data) #expensive, save info and use for later?
+    print(PI.raw_data.keys())
+
+    print('\n-----_internal---------------------------------------------------')
+    print(PI._internal.keys())
+
+    print('\n------league-----------------------------------------------------')
+    print(PI.league.keys())
+
+    print('\n-----standard----------------------------------------------------')
+    #print(PI.standard) #expensive
+
+    print('standard keys')
+    print(PI.standard[0].keys())
