5d1eef2155fd003ffcd5278b7773b3cf7243fc51
Cole <cebCole@gmail.com>
2020-02-05 19:57:39 -0600

    TeamLeaders Object and updates

--- api_functions/nbateamleaders.py
+++ api_functions/nbateamleaders.py
@@ -1,8 +1,5 @@
 from functions import get_data
 
-from nbaplayers import get_player_name
-from nbateam import get_team_url
-
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('.', 'config')))
 from getconfiginfo import get_team, get_season
@@ -10,7 +7,7 @@ from getconfiginfo import get_team, get_season
 MAIN_FIVE_STATS = ['ppg', 'trpg', 'apg', 'bpg', 'spg']
 main_five_stats = []
 
-def get_team_leaders(team=None, season=None):
+def get_team_leaders_data(team=None, season=None):
     """Gets raw json data for stat leaders for given team.
 
     Args:
@@ -21,11 +18,11 @@ def get_team_leaders(team=None, season=None):
         Dict of raw json data from data.nba.net /leaders.json endpoint
     """
     if not team: team = get_team()
-    if not date: date = get_season()
+    if not season: season = get_season()
 
     url1 = 'http://data.nba.net/prod/v1/'
     url_end = '/leaders.json'
-    url = str(url1) + str(date) + '/teams/' + str(team.lower()) + str(url_end)
+    url = str(url1) + str(season) + '/teams/' + str(team.lower()) + str(url_end)
 
     data = get_data(url)
     return data
@@ -35,10 +32,45 @@ class TeamLeaders(object):
     """A class to sort and hold data from NBA's /leaders.json endpoint
 
     Attributes:
+        raw_data: dictionary of raw json data
+        _internal: raw_data key of internal data
+        league: data of various leagues
+        standard: raw_data key, holds data of respective leaders
     """
-    def __init__(self):
-        pass
+    def __init__(self, team=None, season=None):
+        self.team = team
+        self.season = season
+
+        self.raw_data = get_team_leaders_data(team=self.team,
+                                              season=self.season)
+        self._internal = self.raw_data['_internal']
+
+        self.league = self.raw_data['league']
+        self.standard = self.league['standard']
+
+        self.leader_keys = ['ppg','trpg','apg','fgp','tpp','ftp','bpg','spg',
+            'tpg','pfpg']
+        self.leaders = self.standard
+        self.leaders.pop('seasonStageId', None)
+
 
 
 if __name__ == '__main__':
+    TL = TeamLeaders()
+    print('-----RAW DATA BREAKDOWN, DICT KEYS---------------------------------')
+    print(TL.raw_data.keys())
+
+    print('\n-----_internal---------------------------------------------------')
+    print(TL._internal.keys())
+
+    print('\n-----league------------------------------------------------------')
+    print(TL.league.keys())
+
+    print('\n-----standard----------------------------------------------------')
+    print(TL.standard.keys())
+    print(TL.standard)
+    print()
+    print()
+    print(TL.leaders)
+
     pass
