68c6d727378fb7869ee75a68c5c97cd3bf262e82
Cole <cebCole@gmail.com>
2020-02-20 09:09:26 -0600

    ordering change

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ from nba_boxscore_ui import BoxScoreUI
 from nba_schedule_ui import ScheduleUI
 from nba_teamleaders_ui import TeamLeadersUI
 from nba_scores_ui import ScoreBoardUI
+from nba_divstandings_ui import DivStandingsUI
 
 
 if __name__ == '__main__':
-    S = ScheduleUI(next=True, last=True, full=False)
-    S.display()
-
     SB = ScoreBoardUI()
     SB.display(horiz=True)
 
     B = BoxScoreUI()
     B.display()
 
+    S = ScheduleUI(next=True, last=True, full=False)
+    S.display()
+
     TL = TeamLeadersUI()
     TL.display()
+
+    DS = DivStandingsUI()
+    DS.display()
