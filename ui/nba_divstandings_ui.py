d644452a74e9e672d30af3724861d7236a17096b
Cole <cebCole@gmail.com>
2020-02-20 07:38:33 -0600

    div standings work

--- ui/nba_divstandings_ui.py
+++ ui/nba_divstandings_ui.py
@@ -15,6 +15,18 @@ class DivStandingsUI(Widget):
         self.DS = DivStandings(division=division)
         self.TI = TeamInfo()
 
+        self.div_headers = [
+        'Team','W','L','PCT','GB','HOME','AWAY', 'DIV',
+        'CONF','STRK','L10'
+        ]
+
+        self.div_data = [
+        'teamId','win','loss','winPct','divGamesBehind',('homeWin', 'homeLoss'),
+        ('awayWin','awayLoss'), ('divWin', 'divLoss'), ('confWin','confLoss'),
+        ('isWinStreak', 'streak'),('lastTenWin','lastTenLoss')
+        ]
+
+
 
     def display(self, horiz=True):
         pass
@@ -22,13 +34,26 @@ class DivStandingsUI(Widget):
     def create_nested_list(self, data):
         nested_list = []
 
+        """Stats Header"""
+        nested_list.append(self.div_headers)
+
     def get_nested_list(self, data):
         """Calls create_nested_list method in order to get the values for the
         nested list to be passed on to create_tabulate_table method"""
         return self.create_nested_list(data)
 
 
+    def basic_print(self):
+        for team in self.DS.east_conf['atlantic']:
+            for k, v in team.items():
+                print(f"{k}:{v}", end=' ')
+            print()
+            #print(team)
+        #print(self.DS)
+
+
 
 if __name__ == '__main__':
     DS = DivStandingsUI()
     DS.display()
+    DS.basic_print()
