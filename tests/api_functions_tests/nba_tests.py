cdea52bbd464a8c88e0c93c67790378c26956fa1
Cole <cebCole@gmail.com>
2020-04-07 11:35:23 -0500

    adjusted to handle running tests from difference modules/files

--- tests/api_functions_tests/nba_tests.py
+++ tests/api_functions_tests/nba_tests.py
@@ -14,6 +14,9 @@ class TestBoxScore(unittest.TestCase):
     def test_BoxScore_given_date(self):
         BS = BoxScore(date='20200117')
         self.assertEqual(BS.date, '20200117')
+        self.assertEqual(BS.hTeam_totals['points'], '140')
+        self.assertEqual(BS.vTeam_totals['points'], '111')
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,15 @@ from api_functions_tests import functions_tests, nba_tests
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromModule(functions_tests, nba_tests)
+    test_modules_to_run = [functions_tests, nba_tests]
 
-    unittest.TextTestRunner(verbosity=1).run(suite)
+    loader = unittest.TestLoader()
+
+    suites_list = []
+    for test_module in test_modules_to_run:
+        suite = loader.loadTestsFromModule(test_module)
+        suites_list.append(suite)
+
+    big_suite = unittest.TestSuite(suites_list)
+    runner = unittest.TextTestRunner(verbosity=1)
+    results = runner.run(big_suite)
